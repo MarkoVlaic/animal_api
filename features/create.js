97b4a173b12f976c2ca4a3647d4897526309ad3c
MarkoVlaic <vlaic.marko@gmail.com>
2018-05-02 22:46:17 +0200

    Updated create.addSpecies

--- features/create.js
+++ features/create.js
@@ -1,13 +1,28 @@
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
 
+const errorHandler = require('../errorHandler');
+
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+/* Creates a new species in the table with parameters from event.body if there is no species with the same name in the table */
 
 module.exports.addSpecies = (event, context, callback) => {
   const body = JSON.parse(event.body);
   console.log(body);
 
-  const params = {
+  const scanParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    FilterExpression: '#n <> :n',
+    ExpressionAttributeNames: {
+      '#n': 'name'
+    },
+    ExpressionAttributeValues: {
+      ':n': body.name
+    }
+  }
+
+  const insertParams = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
       name: body.name,
@@ -17,17 +32,31 @@ module.exports.addSpecies = (event, context, callback) => {
     }
   };
 
-  dynamodb.put(params, err => {
-    if(err)return callback(JSON.stringify(err), {statusCode: 500, body:{error: JSON.stringify(err)}});
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: `Successfully created ${body.name} species in the database`
-        }
-      )
-    };
+  dynamodb.scan(scanParams, (err, species) => {
+    if(err)errorHandler(err, callback);
+    if(!species || species.Items.length === 0){
+      dynamodb.put(insertParams, err => {
+        if(err)return errorHandler(err, callback)
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(
+            {
+              message: `Successfully created ${body.name} species in the database`
+            }
+          )
+        };
 
-    return callback(null, response);
+        return callback(null, response);
+      });
+    }else {
+      const response = {
+        statusCode: 304,
+        body: JSON.stringify({
+          message: `Species with name ${body.name} already exists`
+        })
+      };
+
+      callback(null, response);
+    }
   });
 }
--- features/read.js
+++ features/read.js
@@ -1,12 +1,42 @@
 const AWS = require('aws-sdk');
+const errorHandler = require('../errorHandler');
 
-module.exports.getSpecies = (event, context, callback) => {
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+/* Returns an array of all the species in the table */
+module.exports.getAllSpecies = (event, context, callback) => {
+  dynamodb.scan({TableName: process.env.DYNAMODB_TABLE}, (err, species) => {
+    if(err)return errorHandler(err, callback);
     const response = {
       statusCode: 200,
-      body: JSON.stringify({
-        message: 'Test passed'
-      })
+      body: JSON.stringify(species.Items)
+    };
+    callback(null, response);
+  });
+}
+
+/* Returns an array of species with name parameter equal to event.pathParameters.species */
+module.exports.getSpecies = (event, context, callback) => {
+    const { species } = event.pathParameters;
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      FilterExpression: `#nm = :n`,
+      ExpressionAttributeNames: {
+        '#nm': 'name'
+      },
+      ExpressionAttributeValues: {
+        ':n': species
+      }
     };
 
-    callback(null, response);
+    dynamodb.scan(params, (err, species) => {
+      if(err)return errorHandler(err, callback);
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(species.Items)
+      };
+      callback(null, response);
+    });
+
 }
